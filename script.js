84be723f5a025f91cb39f1c5f69d956f704b4957
Alexander Rivera <alexandercapeta@gmail.com>
2023-05-27 10:25:58 -0600

    Removing the class from the modal using the classList.remove method.

--- script.js
+++ script.js
@@ -8,5 +8,10 @@ const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModal);
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  console.log(btnsOpenModal[i].addEventListener('click', function () {}));
+  console.log(
+    btnsOpenModal[i].addEventListener('click', function () {
+      //console.log('Button clicked.');
+      modal.classList.remove('hidden');
+    })
+  );
 }
